e74ed4eb7c7357c3678e059b68d685e27b2f7f08
Shinya Sasaki <shinya.sasaki@kek.jp>
2014-03-24 18:55:47 +0900

    refactoring fem/inc/common.h

--- fem/include/common.h
+++ fem/include/common.h
@@ -10,12 +10,13 @@
 #include <math.h>
 #include <float.h>
 
-#define EPS_0 (8.854187817e-12)
-#define CALC_EPS (1e-15)
+#define EPS_0       (8.854187817e-12)
+#define CALC_EPS    (1e-15)
 #define LIGHT_SPEED (299792458)
-#define MU0 (4*M_PI*1e-7)
-#define EEC (1.60217646e-19)
-#define BUFF_SIZE 255
+#define MU0         (4*M_PI*1e-7)
+#define EEC         (1.60217646e-19)
+#define BUFF_SIZE   255
+
 using namespace std;
 
 #include "classes.h"
